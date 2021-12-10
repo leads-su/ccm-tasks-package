ebd16c281e5b56cc5554dd2817ebec092f20bb8c
Иван Живолупов <zhivolupov.i@leads.team>
2021-12-10 15:28:39 +0300

    Updated testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -81,6 +81,34 @@ abstract class TestCase extends \ConsulConfigManager\Testing\TestCase
      */
     public function setUpEnvironment(Application $app): void
     {
+        $this->setConfigurationValue('permission', [
+            'models' => [
+                'permission' => \Spatie\Permission\Models\Permission::class,
+                'role' => \Spatie\Permission\Models\Role::class,
+            ],
+            'table_names' => [
+                'roles' => 'roles',
+                'permissions' => 'permissions',
+                'model_has_permissions' => 'model_has_permissions',
+                'model_has_roles' => 'model_has_roles',
+                'role_has_permissions' => 'role_has_permissions',
+            ],
+            'column_names' => [
+                'role_pivot_key' => 'role_id',
+                'permission_pivot_key' => 'permission_id',
+                'model_morph_key' => 'model_id',
+                'team_foreign_key' => 'team_id',
+            ],
+            'teams' => false,
+            'display_permission_in_exception' => false,
+            'display_role_in_exception' => false,
+            'enable_wildcard_permission' => false,
+            'cache' => [
+                'expiration_time' => \DateInterval::createFromDateString('24 hours'),
+                'key' => 'spatie.permission.cache',
+                'store' => 'default',
+            ],
+        ], $app);
         $this->setConfigurationValue(
             'event-sourcing.snapshot_repository',
             \Spatie\EventSourcing\Snapshots\EloquentSnapshotRepository::class,
